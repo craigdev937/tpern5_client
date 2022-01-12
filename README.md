a321298b5837fea16208f69e7a67b5c6bd4962d6
Craig <indigomx9@gmail.com>
2022-01-11 19:50:46 -0500

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# TPERN5 Client
+
+Initial setup of the project featuring: TypeScript, React, Redux Toolkit, and createEntityAdapter.
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio culpa inventore eius quam est asperiores dolorum quibusdam non veritatis sequi consectetur animi neque aut hic qui accusamus, nesciunt nulla temporibus!</p>
+        </React.Fragment>
+    );
+};
+
+
+
